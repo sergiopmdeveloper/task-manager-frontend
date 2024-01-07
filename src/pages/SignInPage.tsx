7a94d2597e8df49a65a923e9475e08ab987076ef
Sergio <sergio.pm.developer@gmail.com>
2024-01-07 18:48:03 +0000

    feat: check session on sign in page load

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,5 +1,9 @@
-import { SignInForm } from '@/components/app/SignInPage/SignInForm'
 import { PageWrapper } from '@/components/app/Auth/PageWrapper'
+import { SignInForm } from '@/components/app/SignInPage/SignInForm'
+import { useAuth } from '@/hooks/useAuth'
+import { validateSession } from '@/utils/validateSession'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 /**
  * Renders the sign-in page.
@@ -7,9 +11,28 @@ import { PageWrapper } from '@/components/app/Auth/PageWrapper'
  * @returns The sign-in page component.
  */
 export function SignInPage() {
-  return (
-    <PageWrapper>
-      <SignInForm />
-    </PageWrapper>
-  )
+  const user = useAuth()
+  const [userIsLoading, setUserIsLoading] = useState(true)
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (user) {
+      setUserIsLoading(false)
+    }
+  }, [user])
+
+  useEffect(() => {
+    if (!userIsLoading && validateSession(user)) {
+      navigate(`/user/${user.name?.toLowerCase()}`)
+    }
+  }, [userIsLoading, user, navigate])
+
+  if (!userIsLoading && !validateSession(user)) {
+    return (
+      <PageWrapper>
+        <SignInForm />
+      </PageWrapper>
+    )
+  }
 }
