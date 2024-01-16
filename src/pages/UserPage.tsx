68e541e119372d0a9d5bb0fc70f3ed1ed9296d58
Sergio <sergio.pm.developer@gmail.com>
2024-01-16 00:35:29 +0000

    refactor: isolate user header in a component

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,8 +1,7 @@
-import { Button } from '@/components/ui/button'
+import { UserHeader } from '@/components/app/UserPage/UserHeader'
 import { useAuth } from '@/hooks/useAuth'
 import { useValidateUserUrl } from '@/hooks/useValidateUserUrl'
 import { verifyToken } from '@/services/verifyToken'
-import { signOut } from '@/utils/signOut'
 import { validateSession } from '@/utils/validateSession'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
@@ -36,12 +35,9 @@ export function UserPage() {
 
   if (!userIsLoading && validateSession(user)) {
     return (
-      <div className="container h-20 items-center flex justify-between">
-        <h1 className="text-xl font-semibold">Welcome {user.name}</h1>
-        <Button variant="destructive" onClick={() => signOut(navigate)}>
-          Logout
-        </Button>
-      </div>
+      <>
+        <UserHeader name={user!.name} />
+      </>
     )
   }
 }
